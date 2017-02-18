bd32c2d7460a73c4176aecad6d9a7f172d785623
Praveen Chandra Thapliyal <pthapliyal781@gmail.com>
2017-02-18 15:36:27 +0530

    no idea

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,5 +1,14 @@
 
 var app = angular.module('myApp', [] );
-app.controller('meetupsController', function($scope){
-	$scope.meetupsCount = 10;
-});
+app.controller('meetupsController', ['$scope', function($scope){
+	$scope.meetups = [
+	{ name: "This is 1st meetup"},
+	{ name: "This is 2nd meetup"}
+	];
+	console.log("blah");
+	$scope.createMeetup = function() {
+		console.log("blah-blah");
+		$scope.meetups.push({ name: $scope.meetupName });
+		$scope.meetupName = "";
+	};
+}]);
